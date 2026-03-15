ef5ded1c67abd17ba8163b2f081ecabf3a7186e8
Luis Lobato <luislobato.medinaeduardo@gmail.com>
2026-03-15 13:17:07 -0600

    Se ha modificado el id de un elemento, implemente a medias el metodo fetchGames, voy a ir primero con el de los spinners y la implementación del insert, (la modificación del texto del botón insertar a actualizar ya esta hecha solo falta hacer el cambio de metodos y crear esos metodos)

--- app/src/main/java/com/lobato/videogamesmobile/MainActivity.java
+++ app/src/main/java/com/lobato/videogamesmobile/MainActivity.java
@@ -1,6 +1,9 @@
 package com.lobato.videogamesmobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.lobato.videogamesmobile.DTOs.DTOVideogame;
+import com.lobato.videogamesmobile.Services.ApiService;
+import com.lobato.videogamesmobile.View.VideogameForm;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class MainActivity extends AppCompatActivity {
+    public ApiService apiService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +33,28 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+    }
+    public void addGame(View v){
+        startActivity(new Intent(MainActivity.this, VideogameForm.class));
+    }
+    private void fetchAllGames(){
+        Call<List<DTOVideogame>> call= apiService.getAllVideogames();
+        call.enqueue(new Callback<List<DTOVideogame>>() {
+            @Override
+            public void onResponse(Call<List<DTOVideogame>> call, Response<List<DTOVideogame>> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<List<DTOVideogame>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Hubo un error con el servidor",Toast.LENGTH_LONG);
+            }
         });
     }
 }
--- app/src/main/java/com/lobato/videogamesmobile/View/VideogameForm.java
+++ app/src/main/java/com/lobato/videogamesmobile/View/VideogameForm.java
@@ -1,16 +1,27 @@
 package com.lobato.videogamesmobile.View;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.lobato.videogamesmobile.DTOs.DTOVideogame;
 import com.lobato.videogamesmobile.R;
 
 public class VideogameForm extends AppCompatActivity {
 
+    public static DTOVideogame videogame = null;
+
+
+    Button buttonGenre, buttonPlatform,btnImage, btnAddGame;
+    Spinner spGenres, spPlatforms, spEsrb;
+    EditText name, author, price, specs, url, stock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +32,23 @@ public class VideogameForm extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnAddGame = findViewById(R.id.btn_addGame);
+        spGenres = findViewById(R.id.sp_genres);
+        spPlatforms = findViewById(R.id.sp_platform);
+        spEsrb = findViewById(R.id.sp_esrb);
+        buttonGenre = findViewById(R.id.btn_genre);
+        buttonPlatform = findViewById(R.id.btn_platform);
+        btnImage = findViewById(R.id.btn_img);
+        name = findViewById(R.id.etxt_name);
+        author = findViewById(R.id.etxt_author);
+        price = findViewById(R.id.etxt_price);
+        specs = findViewById(R.id.etxt_specs);
+        url = findViewById(R.id.etxt_url);
+        stock = findViewById(R.id.etext_stock);
+        if(videogame != null){
+          btnAddGame.setText("Actualizar videojuego");
+        }
     }
+
+
 }
